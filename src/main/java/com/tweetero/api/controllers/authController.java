ab1530bdf9524f13cd326fe42bd93f2e1f06f0f3
Tallispt <tallispt@gmail.com>
2023-04-03 17:06:49 -0300

    Implemented user service

--- src/main/java/com/tweetero/api/controllers/authController.java
+++ src/main/java/com/tweetero/api/controllers/authController.java
@@ -1,5 +1,6 @@
 package com.tweetero.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,17 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tweetero.api.dtos.UserDTO;
 import com.tweetero.api.models.User;
-import com.tweetero.api.repositories.UserRepository;
+import com.tweetero.api.services.UserService;
 
 
 @RestController
 @RequestMapping("/sign-up")
 public class authController {
-  UserRepository repositoriy = new UserRepository();
+  
+  @Autowired
+  private UserService service;
 
   @PostMapping
   public User insertUser(@RequestBody UserDTO req) {
-    return repositoriy.save(new User(req));
+    return service.save(req);
   }
   
 }
--- src/main/java/com/tweetero/api/services/UserService.java
+++ src/main/java/com/tweetero/api/services/UserService.java
@@ -1,8 +1,20 @@
 package com.tweetero.api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.tweetero.api.dtos.UserDTO;
+import com.tweetero.api.models.User;
+import com.tweetero.api.repositories.UserRepository;
+
 
 @Service
 public class UserService {
-  
+
+  @Autowired(required = false)
+  private UserRepository repository;
+
+  public User save(UserDTO dto){
+    return repository.save(new User(dto));
+  }
 }
